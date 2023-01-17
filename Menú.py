7526bda494bbd863424de4853b4575c53b28c4f9
Carlos Martin <charlimarti1997@hotmail.com>
2023-01-17 17:39:13 +0100

    Menú

---
+++ Menú.py
@@ -0,0 +1,15 @@
+int opcion = 0
+
+while (opcion<4):
+          
+    def mostrarMenu():
+
+    
+        print(int("\nIntroduzca opción (1-4): "))
+          
+                if (opcion==1):
+                    print("Has escogido la opcion 1")
+                    return numerosDelUnoAlDiez()
+
+                else:
+                    print("Has escogido la opcion Salir") 
