7d98fb68bdb53e9cb93d2676eb53150095a69f5e
Eduard Gomá <edugb1408@gmail.com>
2023-01-17 20:29:24 +0100

    Creado metodo almacena una coleccion de diccionarios en una lista

--- gestion_juegos.py
+++ gestion_juegos.py
@@ -4,12 +4,23 @@ def get_CSV():
     with open('juegos.csv', newline='') as csvfile:
         lista_csv = list(csv.reader(csvfile))
     return lista_csv
-print(get_CSV())
+lista = get_CSV()
+columnas = lista[0]
+diccionario = {}
+lista2 = []
+for i in range(len(lista)):
+    datos = zip(columnas,lista[i])
+    diccionario = (dict(datos))
+    lista2.append(diccionario)
 
-with open('example.csv', 'w', newline='') as csvfile:
+print(lista2[1])
+
+
+"""with open('example.csv', 'w', newline='') as csvfile:
     fieldnames = ['name', 'age']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
     writer.writeheader()
     for fila in lista_csv:
-        writer.writerow(fila)
+        writer.writerow(fila)"""
+        
