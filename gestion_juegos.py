ad70f947b487a900444c48af3ada39f616a54b9d
Eduard Gomá <edugb1408@gmail.com>
2023-01-18 12:20:46 +0100

    Modificada funcion add_game y creada funcion shorted ordena las nuevas filas y las añade al csv por orden de ventas globales

--- gestion_juegos.py
+++ gestion_juegos.py
@@ -27,26 +27,43 @@ def get_dict():
 # Esta funcion permite añadir juegos nuevos
 
 
+
 def add_games():
-    columnas = ['Rank', 'Name', 'Platform', 'Year', 'Genre', 'Publisher',
-                'NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales',
-                'Global_Sales']
-    lista_asubir = []
-    # Bucle para solicitar datos a añadir
-    for i in range(len(columnas)):
-        lista_asubir.append(input("Introduce el {}: ".format(columnas[i])))
-        # Permite escribir nuevas lineas en el csv
 
+# Bucle para solicitar datos a añadir
+    true = True
+    while true:
+        fieldnames = ['Name', 'Platform', 'Year', 'Genre', 'Publisher',
+                'NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales']
+        lista_asubir = []
+        lista_asubir.append("")
+        global_sales = 0
+        for i in range(len(fieldnames)):
+                lista_asubir.append(input("Introduce el {}: ".format(fieldnames[i])))
+                if fieldnames[i] == 'NA_Sales' or fieldnames[i] == 'EU_Sales' or fieldnames[i] == 'JP_Sales' or fieldnames[i] == 'Other_Sales':
+                    global_sales += float(lista_asubir[i])
+        lista_asubir.append(global_sales)
+                # Permite escribir nuevas lineas en el csv
+        with open("juegos.csv", "a", newline="", encoding="utf-8") as csvfile:
+                csv_writer = csv.writer(csvfile)
+                csv_writer.writerow(lista_asubir)
 
-"""    with open("juegos.csv", "w", newline="", encoding="utf-8") as csvfile:
-        writer = csv.DictWriter(csvfile, columnas=columnas)
-        writer.writeheader()
-        for fila in lista_asubir:
-            writer.writerow(fila)
-"""
-
-
-# La función devuelve el diccionario del juego solicitado
+def sorted_games():
+    csv2 = get_CSV()
+    lista_csv = []
+    for i in range(1,len(csv2)):
+        lista_csv.append(csv2[i])
+    diccionario = ['Rank', 'Name', 'Platform', 'Year', 'Genre', 'Publisher', 'NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales', 'Global_Sales']
+    sorted_list = sorted(lista_csv, key=lambda x: float(x[10]), reverse= True)
+    for i in range(len(sorted_list)):
+        sorted_list[i][0] = i+1
+    sorted_list = sorted_list[::-1]
+    sorted_list.append(diccionario)
+    sorted_list = sorted_list[::-1]
+    with open("juegos.csv", "w", newline="", encoding="utf-8") as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerows(sorted_list)
+sorted_games()
 
 
 # La función devuelve los 5 juegos más vendidos en el mundo
