3f1277f82ce46c25945be8e853c7444bfe263c28
Carlos Martin <charlimarti1997@hotmail.com>
2023-01-18 09:48:29 +0100

    Nuevo menú

--- menu.py
+++ menu.py
@@ -4,22 +4,25 @@ def pedirNumero():
     num = 0
     while (not correcto):
         try:
-            print("1. Opción 1")
-            print("2. Opción 2")
-            print("3. Opción 3")
-            print("4. Salir")
-            num = int(input("Introduce un número entero: "))
+            print("1) Búsqueda de juegos")
+            print("2) Añadir juegos")
+            print("4) Salir")
+            num = int(input("Introduce una opción: "))
             correcto = True
 
         except ValueError:
             print('Error, introduce un número entero')
 
         if num == 1:
-            print("Opción 1")
+            print("=======Buscar juego=======")
+            from gestion_juegos import get_CSV
+        elif num == 2:
+            print("=======Añadir juego=======")
+            from gestion_juegos import add_games
         elif num == 4:
             correcto = True
         else:
-            print("Introduce un número 1 o 4")
+            print("Introduce una opción")
             correcto = False
 
 
