91ee80c5ac97a0afdc8eac02dfbe022c8beb1a85
Eloy Gracia <eloygb2000@gmail.com>
2023-01-18 11:48:00 +0100

    Por favor, te lo suplico, arregla el repo

--- buscador.py
+++ buscador.py
@@ -1,19 +1,53 @@
 import gestion_juegos
 
 
+def filtrado_nombre(termino_a_buscar):
+
+    diccionario_juego = []
+    lista_aux = gestion_juegos.get_dict()
+    for juego in range(len(lista_aux)):
+        if lista_aux[juego]["Name"] == termino_a_buscar:
+            # print(lista_aux[i])
+            diccionario_juego = lista_aux[juego]
+
+    return diccionario_juego
+
+
+def filtrado_genero(termino_a_buscar):
+
+    lista_aux = gestion_juegos.get_dict()
+    print("La lista de juegos del género",
+          termino_a_buscar, "es: ")
+    for juego in range(len(lista_aux)):
+        if lista_aux[juego]["Genre"] == termino_a_buscar: 
+            print(lista_aux[juego]["Name"])
+    return "\nPrograma finalizado"
+
+
 def termino_busqueda():
 
-    Salir = False
-    while (not Salir):
+    salir = False
+    eleccion = filtrar_por()
+    while (not salir):
         try:
-            nombre = str(input("Introduce un término de búsqueda: "))
-            valoresfiltrado = filtrado_nombre(nombre)
+            termino_a_buscar = str(input("Introduce un término de búsqueda: "))
 
-            """Si el parámetro introducido en filtrado_nombre() no se encuentra
+            if eleccion == "1":
+                valoresfiltrados = filtrado_nombre(termino_a_buscar)
+                salir = True
+
+                # Si no se mete ninguna función de búsqueda adicional, cambiar a else
+            if eleccion == "2":
+                valoresfiltrados = filtrado_genero(termino_a_buscar)
+                salir = True
+
+            # valoresfiltrados = filtrado_genero(termino_a_buscar)
+
+            """Si el parámetro introducido en filtrado_termino_a_buscar() no se encuentra
             en el diccionario, la longitud devuelta es igual a 0"""
 
-            if len(valoresfiltrado) == 0:
-                if nombre == '':
+            if len(valoresfiltrados) == 0:
+                if termino_a_buscar == '':
                     print("No se ha introducido texto")
                     termino_busqueda()  # Se vuelve a pedir la entrada
 
@@ -24,12 +58,13 @@ def termino_busqueda():
 
                 while not salir_subbucle:
                     add_game_prompt = str(input(
-                        "\nEl juego no se encuentra en la base" +
-                        " de datos,¿Te gustaría añadirlo?\n1. Sí\n2. No"
+                        "\nNo se encuentra ningún juego con estas" +
+                        " características en la base de datos," +
+                        " ¿Te gustaría añadirlo?\n1. Sí\n2. No"
                         + "\n3. Realizar otra búsqueda "))
                     if add_game_prompt == '1':
                         salir_subbucle = True
-                        # add_games()
+                        gestion_juegos.add_games()
                     elif add_game_prompt == '2':
                         print("¡Hasta luego!")
                         salir_subbucle = True
@@ -40,31 +75,33 @@ def termino_busqueda():
                     else:
                         print("\nIntroduce un valor correcto")
             else:
-                print(valoresfiltrado)
-            Salir = True
+                print(valoresfiltrados)
+                salir = True
+
         except Exception:
             print('Error, revisa la entrada e inténtalo de nuevo.')
 
 
-def filtrado_nombre(nombre):
+def filtrar_por():
+    salir = False
+    try:
+        while not salir:
+            eleccion = str(input("\n¿Por qué quieres buscar?" +
+                                 "\n1. Por nombre"
+                                 + "\n2. Por género "))
+            if eleccion == "1":
+                print("\n")
+                print("Has seleccionado búsqueda por nombre")
+                salir = True
 
-    diccionario_juego = []
-    lista_aux = gestion_juegos.get_dict()
-    for i in range(len(lista_aux)):
-        if lista_aux[i]["Name"] == nombre:
-            # print(lista_aux[i])
-            diccionario_juego = lista_aux[i]
+            if eleccion == "2":
+                print("\n")
+                print("Has seleccionado búsqueda por género")
+                salir = True
 
-    return diccionario_juego
+    except ValueError:
+        print("Se ha producido un error, inténtalo de nuevo")
+    return eleccion
 
 
-def filtrado_genero(nombre):
-
-    diccionario_juego = []
-    lista_aux = gestion_juegos.get_dict()
-    for i in range(len(lista_aux)):
-        if lista_aux[i]["Genre"] == nombre:
-            # print(lista_aux[i])
-            diccionario_juego = lista_aux[i]
-
-    return diccionario_juego
+termino_busqueda()
