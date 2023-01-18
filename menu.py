5116974e14e0467ea056a89b20c744adcfa37e13
Carlos Martin <charlimarti1997@hotmail.com>
2023-01-18 19:32:07 +0100

    Menú mejoras visuales

--- menu.py
+++ menu.py
@@ -1,34 +1,28 @@
-import buscador
 import gestion_juegos
+import buscador
 
 
 def pedirNumero():
-
     correcto = False
     num = 0
     while (not correcto):
         try:
+            print("\n    ******LUCASTEAM******\n")
             print("1) Búsqueda de juegos")
             print("2) Añadir juegos")
-            print("4) Salir")
-            num = int(input("Introduce una opción: "))
-            correcto = True
-
+            print("3) Salir ")
+            num = int(input("\n--> Introduce una opción: "))
+            if num == 1:
+                print("\n=======Buscar juego=======\n")
+                buscador.buscar()
+            elif num == 2:
+                print("\n=======Añadir juego=======\n")
+                gestion_juegos.add_games()  
+            elif num == 3:
+                correcto = True
+            else:
+                print("\n***Introduce una opción válida***\n")
+                correcto = False
         except ValueError:
-            print('Error, introduce un número entero')
-
-        if num == 1:
-            print("=======Buscar juego=======")
-            from buscador import buscar
-            buscar()
-        elif num == 2:
-            print("=======Añadir juego=======")
-            from gestion_juegos import add_games
-            add_games()
-        elif num == 4:
-            correcto = True
-        else:
-            print("Introduce una opción")
-            correcto = False
-
+            print("***Error, introduce un número entero***\n")
 
