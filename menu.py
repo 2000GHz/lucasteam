b280e904be228d896494e230e229c8de38ba56b4
Eduard Gomá <edugb1408@gmail.com>
2023-01-19 19:34:06 +0100

    menu arreglado

--- menu.py
+++ menu.py
@@ -5,10 +5,9 @@ from tabulate import tabulate
 
 
 def pedirNumero():
-    correcto = False
-    num = 0
+    correcto = True
     os.system("cls")
-    while (not correcto):
+    while correcto:
         try:
             col_name = ["\n  ******LUCASTEAM******\n"]
             data = [["1 · Líbrería de juegos"],
@@ -21,30 +20,25 @@ def pedirNumero():
             if num == 1:
                 print("\n=======Listar CSV=======\n")
                 os.system("cls")
-                correcto = True
                 print("\n=======Listar CSV=======\n")
                 n = int(input("*** Introduce el numero" +
                               " de filas a mostrar ***\n"))
                 print("\n")
                 gestion_juegos.list_all_csv(n)
                 os.system("cls")
-                pedirNumero()
-
             elif num == 2:
                 print("\n=======Buscar juego=======\n")
                 os.system("cls")
-                correcto = True
                 buscador.buscar()
             elif num == 3:
                 print("\n=======Añadir juego=======\n")
                 os.system("cls")
                 gestion_juegos.add_games()
             elif num == 4:
-                correcto = True
+                correcto = False
                 os.system("cls")
             else:
                 print("\n*** Introduce una opción válida ***\n")
-                correcto = False
                 os.system("cls")
         except ValueError:
             os.system("cls")
